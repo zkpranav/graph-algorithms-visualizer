dfbb96d971a92bd3fd686b85b286c0c64b32895f
zkpranav <zkpranav@icloud.com>
2021-09-03 16:10:13 +0530

    Adding slide in animation to sidebar

--- src/Sidebar/Sidebar.jsx
+++ src/Sidebar/Sidebar.jsx
@@ -2,14 +2,25 @@ import './Sidebar.css'
 
 import React from 'react'
 import Info from '../Info/Info.jsx'
+import { gsap } from 'gsap'
 
 function Sidebar(props) {
+    /**
+     * ref variables
+     */
+    const sidebar = React.createRef()
+
     function handleToggleSidebar(e) {
-        document.querySelector('#sidebar').style.display = 'flex'
+        sidebar.current.style.display = 'flex'
+        gsap.from(sidebar.current, {
+            x: -sidebar.current.clientWidth,
+            duration: 0.5,
+            ease: 'power3.out'
+        })
     }
 
     function handleMenuCollapse(e) {
-        document.querySelector('#sidebar').style.display = 'none'
+        sidebar.current.style.display = 'none'
     }
 
     return (
@@ -18,7 +29,9 @@ function Sidebar(props) {
                 id='toggle-sidebar'
                 onClick={handleToggleSidebar}
             >&#9776;</button>
-            <div id='sidebar'>
+            <div 
+                ref={sidebar}
+                id='sidebar'>
                 <button 
                     id='menu-collapse' 
                     onClick={handleMenuCollapse}
@@ -32,11 +45,11 @@ function Sidebar(props) {
                 <ol id='tutorial' >
                     <li>
                     Set Vertices of the Graph<br />
-                    To add a Vertex, Simply tap
+                    To add a Vertex, Simply Click in the rectangular area
                     </li>
                     <li>
                         Set Edges of the Graph<br />
-                        To set and edge, first tap the Source-Vertex and then the Destination-Vertex
+                        To set and edge, first click the Source-Vertex and then the Destination-Vertex
                     </li>
                     <li>
                         Finally, select an Algorithm and begin
